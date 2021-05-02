ba4e40965a08bc4ac93b269cc1624f1e9e157132
Verner Codoceo <vacodoceo@uc.cl>
2021-05-01 21:52:10 -0400

    Fix routes and add manifest icons

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -16,7 +16,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={(props) =>
-        user ? <Component {...props} /> : <Redirect to="/" />
+        user ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
