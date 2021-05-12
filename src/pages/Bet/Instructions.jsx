36fb56998294c161bf6536142b24b9154b6228fb
Verner Codoceo <vacodoceo@uc.cl>
2021-05-12 00:45:50 -0400

    Update instructions

--- src/pages/Bet/Instructions.jsx
+++ src/pages/Bet/Instructions.jsx
@@ -4,11 +4,11 @@ export const instructionsSteps = {
   'Fase de Grupos':
     'Debes ingresar los resultados de la fase de grupo, para ello debes agarrar y arrastrar los equipos de cada grupo hasta que queden en el orden que creas.',
   'Cuartos de Final':
-    'Después en la pestaña de Cuartos de Final, para el primer y cuarto encuentro, debes seleccionar cuales serán los dos mejores terceros de los grupos A, B y C. Posteriormente debes ingresar los goles del tiempo reglamentario para cada partido, en caso que apuestes por un empate deberás marcar en el cuadrado correspondiente qué equipo crees que pasará de ronda.',
+    'Después, en la pestaña de cuartos de final, debes ingresar los goles del tiempo reglamentario para cada partido. En caso que apuestes por un empate deberás marcar en la corona correspondiente qué equipo crees que pasará de ronda.',
   Semifinal:
-    'Una vez ingresado todos los resultados de los Cuartos de Final se habilitará la pestaña de la Semifinal, donde deberás seleccionar los goles del tiempo reglamentario para cada semifinal, en caso que apuestes por un empate deberás marcar en el cuadrado correspondiente qué equipo crees que pasará de ronda.',
+    'Una vez ingresado todos los resultados de los Cuartos de Final, podrás avanzar a la fase de las Semifinales. Aquí deberás seleccionar los goles del tiempo reglamentario para cada semifinal, en caso que apuestes por un empate deberás marcar la corona correspondiente qué equipo crees que pasará de ronda.',
   Final:
-    'Finalmente se habilitará la pestaña Final donde podrás ingresar los resultados de la final y el partido por el tercer lugar. En caso que apuestes un empate deberás marcar en el cuadrado correspondiente qué equipo crees que pasará de ronda.',
+    'Finalmente se habilitará la pestaña Final, donde podrás ingresar los resultados de la final y el partido por el tercer lugar.',
   '¿Quedaste con dudas?':
     'Revisa nuestras preguntas frecuentes o nuestras bases y reglas.',
 };
