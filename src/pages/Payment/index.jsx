f41f22b038bb847c4625aac0f21572f83d64dfbc
Verner Codoceo <vacodoceo@uc.cl>
2021-05-11 21:39:51 -0400

    Add payment summary

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import {
   Button,
   CircularProgress,
   Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
   makeStyles,
+  Paper,
+  Typography,
 } from '@material-ui/core';
 import _ from 'lodash';
 
@@ -16,33 +23,64 @@ function useQuery() {
 }
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    paddingTop: theme.spacing(2),
+  button: {
+    justifySelf: 'end',
+  },
+  listItem: {
+    padding: theme.spacing(1, 0),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'grid',
+  },
+  paymentMethod: {
+    '& em': {
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+    },
   },
   progress: {
     marginRight: theme.spacing(1),
   },
+  root: {
+    paddingTop: theme.spacing(2),
+  },
+  title: {
+    marginTop: theme.spacing(2),
+  },
+  total: {
+    fontWeight: 700,
+  },
 }));
+
+const getPrice = (quantity) => {
+  if (quantity < 2) return 3000;
+  else if (quantity < 7) return 2500;
+  return 2000;
+};
 
 const Payment = () => {
   const query = useQuery();
   const history = useHistory();
+
+  const [pollas] = useState(query.get('pollas')?.split(','));
   const [checkout, setCheckout] = useState();
   const [checkoutError, setCheckoutError] = useState();
-  const [pollas, setPollas] = useState([]);
   const [feedbackDialogOpen, setFeedbackDialogOpen] = useState(false);
+  const [pollasData] = useCollectionData(
+    firebase.firestore().collection('pollas').where('__name__', 'in', pollas),
+    { idField: 'id' }
+  );
 
   useEffect(() => {
-    const newPollas = query.get('pollas')?.split(',');
-    if (_.isEmpty(newPollas)) {
+    if (_.isEmpty(pollas)) {
       history.push('/');
     }
-
-    setPollas(newPollas);
-  }, []);
+  }, [pollas]);
 
   useEffect(async () => {
     if (!_.isEmpty(pollas)) {
+      console.log(pollas);
       const { data: preferenceId } = await firebase
         .functions()
         .httpsCallable('createPreference')({
@@ -90,29 +128,71 @@ const Payment = () => {
 
   const classes = useStyles();
   return (
-    <Container maxWidth="lg" className={classes.container}>
-      <Button
-        onClick={handlePay}
-        variant="contained"
-        color="primary"
-        disabled={!checkout && !checkoutError}
-      >
-        {!checkout && !checkoutError && (
-          <CircularProgress
-            className={classes.progress}
-            color="secondary"
-            size={20}
-          />
-        )}{' '}
-        Pagar
-      </Button>
-      <FeedbackDialog
-        description={FeedbackDialogDescription}
-        dialogOpen={feedbackDialogOpen}
-        status="error"
-        title="Hubo un error al procesar tu pago"
-        handleClose={() => setFeedbackDialogOpen(false)}
-      />
+    <Container maxWidth="sm" className={classes.root}>
+      <Paper className={classes.paper}>
+        <Typography variant="h6" gutterBottom>
+          Resumen de pago
+        </Typography>
+        <List disablePadding>
+          {pollasData?.map((polla) => (
+            <ListItem className={classes.listItem} key={polla.id}>
+              <ListItemText primary={polla.name} secondary={polla.id} />
+              <Typography variant="body2">
+                ${getPrice(pollas.length).toLocaleString('de-DE')}
+              </Typography>
+            </ListItem>
+          ))}
+
+          <ListItem className={classes.listItem}>
+            <ListItemText primary="Total" />
+            <Typography variant="subtitle1" className={classes.total}>
+              $
+              {(getPrice(pollas.length) * pollas.length).toLocaleString(
+                'de-DE'
+              )}
+            </Typography>
+          </ListItem>
+        </List>
+        <Typography variant="h6" gutterBottom className={classes.title}>
+          Medio de pago
+        </Typography>
+        <Typography
+          gutterBottom
+          variant="body2"
+          className={classes.paymentMethod}
+          align="justify"
+        >
+          Nuestro único medio de pago mediante esta plataforma es{' '}
+          <em>Mercado Pago</em>. Si tienes dudas sobre como funciona este método
+          o prefieres transferirnos el dinero directamente, no dudes en
+          contactarnos por correo (ti@trabajosproyecta.cl) o instagram
+          (@proyectauc)
+        </Typography>
+
+        <Button
+          onClick={handlePay}
+          variant="contained"
+          color="primary"
+          disabled={!checkout && !checkoutError}
+          className={classes.button}
+        >
+          {!checkout && !checkoutError && (
+            <CircularProgress
+              className={classes.progress}
+              color="secondary"
+              size={20}
+            />
+          )}
+          Pagar
+        </Button>
+        <FeedbackDialog
+          description={FeedbackDialogDescription}
+          dialogOpen={feedbackDialogOpen}
+          status="error"
+          title="Hubo un error al procesar tu pago"
+          handleClose={() => setFeedbackDialogOpen(false)}
+        />
+      </Paper>
     </Container>
   );
 };
